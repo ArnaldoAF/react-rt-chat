f5c254543cb4154cf6947f0adf27cd2788ed451b
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-02-09 23:12:16 -0300

    fix: Login Component - minor changes

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -3,9 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -61,10 +58,6 @@ export default function LogIn() {
                         type="password"
                         id="password"
                         autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
                     />
                     <Button
                         type="submit"
